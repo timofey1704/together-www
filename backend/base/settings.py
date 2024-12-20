6e4dbc3d466a38b96a1ba694c634d8185d785b32
Timofey <timofey.syr17@gmail.com>
2024-12-20 14:58:24 +0300

    TOG-10 chage db provider from postgres to mysql

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -92,12 +92,15 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.mysql",
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"), 
+        "PORT": os.environ.get("DB_PORT"),  
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
     }
 }
 
